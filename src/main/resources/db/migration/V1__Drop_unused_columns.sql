82c23cbf0e071bc08261ee734f16fb9ba3e77b82
GitGlimmer <yhb20201019@163.com>
2026-04-28 14:55:32 +0800

    删除无用的SQL文件和IDE配置

--- src/main/resources/db/migration/V1__Drop_unused_columns.sql
+++
@@ -1,8 +0,0 @@
--- 删除MySQL中Product表的多余字段
--- 这些字段已经移动到MongoDB和ClickHouse中
-
-ALTER TABLE product 
-DROP COLUMN IF EXISTS rating,
-DROP COLUMN IF EXISTS review_count,
-DROP COLUMN IF EXISTS view_count,
-DROP COLUMN IF EXISTS favorite_count;
